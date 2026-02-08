ffabcd071ccc0dae95e028a7bd7ea4ac5222bbfd
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-02-08 17:45:10 +0530

    Project Structure Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h5 className="text-red-700">HI My Name is Aditya</h5>
+    </>
+  )
+}
+
+export default App
